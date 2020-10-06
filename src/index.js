3135436a4ad8f19d78d2e46857e7aadd977b2ac0
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-10-06 15:53:53 -0500

    first React project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,132 @@
+// Access to the API
 import React from 'react';
+//Gives us access to the DOM
 import ReactDOM from 'react-dom';
+//Links the CSS file that we have
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+
+//Right-click on any element on the page, click “Inspect” to open the developer tools, and the React tabs (“⚛️ Components” and “⚛️ Profiler”) will appear as the last tabs to the right. Use “⚛️ Components” to inspect the component tree. When yopu click on a square you will see the state change from null to X
+
+//Instead, the best approach is to store the game’s state in the parent Board component instead of in each Square. The Board component can tell each Square what to display by passing a prop, just like we did when we passed a number to each Square.
+
+//To collect data from multiple children, or to have two child components communicate with each other, you need to declare the shared state in their parent component instead. The parent component can pass the state back down to the children by using props; this keeps the child components in sync with each other and with the parent component.
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            //initializes child state as null to begin with
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
+
+        // state = {
+        //     value = null,
+        // }
+
+    }
+    //instance method of class board
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        if(calculateWinner(squares) || squares[i]){
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            //toggle the boolean
+            xIsNext: !this.state.xIsNext,
+        })
+    }
+    renderSquare(i) {
+        //This is how you pass a prop (value) to a method (rendersquare)
+        //Notice how this returns square, this makes Board the parent
+        //we’ll pass down a function from the Board to the Square, and we’ll have Square call that function when a square is clicked.
+        return (<Square value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+
+        />
+        );
+    }
+
+    render() {
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) { status = 'Winner: ' + winner; } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
